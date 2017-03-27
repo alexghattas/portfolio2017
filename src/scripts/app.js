80a6f1e752965af7466470a8fec15eb8ea0618b0
Alex Ghattas <ghattas.alex@gmail.com>
2017-03-26 23:18:13 -0400

    added menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-		$('.introductionText').each(function(){
+		$('.introductionText, .portfolioItems__one--content, .portfolioItems__two--content, .portfolioItems__three--content').each(function(){
 		var imagePos = $(this).offset().top;
 
 		var topOfWindow = $(window).scrollTop();
@@ -9,8 +9,39 @@ $(window).scroll(function() {
 		});
 	});
 
+$(window).scroll(function() {
+		$('.portfolioItems__one--image, .portfolioItems__three--image').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideRight");
+			}
+		});
+	});
+
+
+$(window).scroll(function() {
+		$('.portfolioItems__two--image').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideLeft");
+			}
+		});
+	});
+
 $(window).scroll(function () {
-        $('.mainNavigation__logo').toggleClass("mainNavigation__logo--background ", ($(window).scrollTop() > 200));
-     });
+	$('.mainNavigation__logo').toggleClass("mainNavigation__logo--background ", ($(window).scrollTop() > 200));
+});
 
 
+
+
+$(".hamburgerMenu").click(
+  function() {
+    $(".hamburgerMenu__list").toggleClass("disBtn");
+    $(".hamburgerMenu__list").toggleClass("fadeInTwo");
+  }
+);
